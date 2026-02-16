478b0a518910ee8b0ea11d2c17afd6dabfedeb4c
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-02-15 23:06:01 -0700

    02-dashboard - Add pokemon to favorite

--- projects/02-my-dashboard/src/store/pokemons/pokemons.ts
+++ projects/02-my-dashboard/src/store/pokemons/pokemons.ts
@@ -1,5 +1,5 @@
 import { SimplePokemon } from '@/pokemons';
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface PokemonsState {
     [key: string]: SimplePokemon;
@@ -15,9 +15,20 @@ const initialState: PokemonsState = {
 const pokemonsSlice = createSlice({
   name: 'pokemons',
   initialState,
-  reducers: {}
+  reducers: {
+    toggleFavorite: (state, action: PayloadAction<SimplePokemon>) => {
+        const pokemon: SimplePokemon = action.payload;
+        const { id } = pokemon;
+        if (!!state[id]) {
+            delete state[id];
+            return;
+        } 
+
+        state[pokemon.id] = pokemon;
+    }
+  }
 });
 
-export const {} = pokemonsSlice.actions
+export const { toggleFavorite } = pokemonsSlice.actions
 
 export default pokemonsSlice.reducer
