e0fb27fbb028a6a73ed323a1653119fa04a34f80
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-02-22 20:32:19 -0700

    03-admin-todo - Generate seed endpoint

---
+++ projects/03-admin-todo/app/api/seed/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET() {
+    await prisma.todo.createMany({
+        data: [
+            {
+                description:"Default description",
+                completed: false,
+            },
+            {
+                description: "Another default description",
+                completed: true,
+            },
+            {
+                description: "Yet another default description",
+            },
+            {
+                description: "One more default description",
+            }
+        ]
+    });
+
+    return NextResponse.json({
+        message: "Seeded successfully",
+    });
+}
