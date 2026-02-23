602e61dccc0a111d3fff0b9c3d6476e6da6aaed4
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-02-22 20:17:27 -0700

    03-admin-todo - Initialize prisma

---
+++ projects/03-admin-todo/prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+const todoData: Prisma.TodoCreateInput[] = [
+  {
+    id: "1",
+    description: "Buy groceries",
+    completed: false,
+  },
+  {
+    id: "2",
+    description: "Walk the dog",
+    completed: true,
+}
+];
+
+export async function main() {
+  for (const u of todoData) {
+    await prisma.todo.create({ data: u });
+  }
+}
+
+main();
