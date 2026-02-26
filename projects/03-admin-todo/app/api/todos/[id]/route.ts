84152d428801e010c1c2f89437b86e3be9fb13d3
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-02-25 22:35:11 -0700

    03-admin-todo - Update Todo by id

--- projects/03-admin-todo/app/api/todos/[id]/route.ts
+++ projects/03-admin-todo/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma'
 import { NextResponse } from 'next/server'
+import * as yup from 'yup'
 
 interface RouteParams {
     id: string
@@ -20,4 +21,40 @@ export async function GET(
         return NextResponse.json({ message: `Todo ${id} not found` }, { status: 404 })
     }
     return NextResponse.json({ todo }, { status: 200 })
+}
+
+const putSchema = yup.object({
+    description: yup.string().optional(),
+    completed: yup.boolean().optional(),
+})
+export async function PUT(
+    request: Request,
+    { params }: { params: Promise<RouteParams> }
+) {
+    const { id } = await params
+    const existingTodo = await prisma.todo.findUnique({ where: { id } })
+    if (!existingTodo) {
+        return NextResponse.json({ message: `Todo ${id} not found` }, { status: 404 })
+    }
+
+    try {
+        const { description, completed } = await putSchema.validate(await request.json(), { abortEarly: false })
+
+        const updatedTodo = await prisma.todo
+            .update({
+                where: { id },
+                data: {
+                    description,
+                    completed,
+                },
+            })
+        return NextResponse.json({ updatedTodo }, { status: 200 })
+    } catch (error) {
+        if (error instanceof yup.ValidationError) {
+            return NextResponse.json({ errors: error.errors }, { status: 400 });
+        }
+        console.error('Error validating request body:', error);
+        return NextResponse.json({ error: 'Failed to validate request body' }, { status: 500 });
+    }
+    
 }
