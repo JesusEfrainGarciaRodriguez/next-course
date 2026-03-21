0c2762a499e8bba69960072b3dce65dc7485e4fb
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-03-21 14:08:37 -0700

    03-admin-todo - user todo relation

--- projects/03-admin-todo/src/app/api/seed/route.ts
+++ projects/03-admin-todo/src/app/api/seed/route.ts
@@ -1,8 +1,30 @@
 import { NextResponse } from "next/server";
 import prisma from "@/src/lib/prisma";
+import bcrypt from "bcryptjs";
 
 export async function GET() {
-    await prisma.todo.createMany({
+
+    await prisma.todo.deleteMany(); // delete * from todo
+    await prisma.user.deleteMany(); // delete * from todo
+
+    await prisma.user.create({
+        data: {
+        email: 'test1@google.com',
+        password: bcrypt.hashSync('123456'),
+        roles: ['admin','client','super-user'],
+        todos: {
+            create: [
+            { description: 'Piedra del alma', completed: true },
+            { description: 'Piedra del poder' },
+            { description: 'Piedra del tiempo' },
+            { description: 'Piedra del espacio' },
+            { description: 'Piedra del realidad' },
+            ]
+        }
+        }
+    });
+
+    /* await prisma.todo.createMany({
         data: [
             {
                 description:"Default description",
@@ -19,7 +41,7 @@ export async function GET() {
                 description: "One more default description",
             }
         ]
-    });
+    }); */
 
     return NextResponse.json({
         message: "Seeded successfully",
