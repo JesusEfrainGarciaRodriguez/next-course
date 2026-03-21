d2e870f9886a95c099d69cf89d00e330f24d05d4
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-03-20 22:22:14 -0700

    03-admin-todo - Add Credentials provider

--- projects/03-admin-todo/src/app/api/auth/[...nextauth]/route.ts
+++ projects/03-admin-todo/src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from "@/src/lib/prisma";
+
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { signInEmailPassword } from "@/src/app/auth/actions/auth-actions";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -14,6 +18,27 @@ export const authOptions: NextAuthOptions = {
         GithubProvider({
             clientId: process.env.GITHUB_ID ?? '',
             clientSecret: process.env.GITHUB_SECRET ?? '',
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Correo electrónico", type: "text", placeholder: "correo@example.com" },
+                password: { label: "Contraseña", type: "password", placeholder: "*********" }
+            },
+            async authorize(credentials, req) {
+            // Add logic here to look up the user from the credentials supplied
+            const user = await signInEmailPassword(credentials!.email, credentials!.password)
+
+            if (user) {
+                // Any object returned will be saved in `user` property of the JWT
+                return user
+            } else {
+                // If you return null then an error will be displayed advising the user to check their details.
+                return null
+
+                // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+            }
+            }
         }),
     ],
     session: {
